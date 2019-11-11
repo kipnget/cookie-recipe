48af5d75832ec8bf59b5a17618b612aad9adc596
MUTAI <yegoallen@gmail.com>
2019-11-11 10:13:05 +0300

    this commit adds all files to the project

---
+++ IdeaProjects/CaesarCipher/src/main/java/App.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class App {
+    public String getGreeting() {
+        return "Hello world.";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+
+        Scanner input = new Scanner(System.in);
+        //word
+        System.out.println("enter a sentence or a word");
+        String stringUserEntered = input.nextLine();
+        //key
+        System.out.println("enter a number as key");
+        Integer key = input.nextInt();
+
+        Encode encode = new Encode(key, stringUserEntered);
+        String encodedString = encode.encodeWord(key, stringUserEntered);
+        System.out.println(encodedString);
+
+        ///DECODE THE TOP SECRET MESSAGE
+        //key
+        System.out.println("enter THE number as key");
+        Integer decodeKey = input.nextInt();
+
+        Decode decode = new Decode(key, encodedString);
+        String decodedString = decode.decodeWord(key, stringUserEntered);
+        System.out.println("the decoded string is: " + decodedString);
+
+
+    }
+}
